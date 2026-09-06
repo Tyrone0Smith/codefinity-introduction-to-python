a6797cae9f91a15c63728ee035278cfe45007627
Tyrone0Smith <>
2026-09-06 13:58:37 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,7 @@
+# Define a variable for total cost of an item
+total_cost = 25.00
+
+# a boolean variable to check for discount elegibility
+discountEligible = total_cost >= 20.00
+
+print(f"Is the purchase eligible for a discount? {discountEligible}")
